64dd735a9ce864eba363a78771497623e670af7b
raya009 <150877907+raya009@users.noreply.github.com>
2023-11-14 21:11:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ from PIL import Image
 #streamlit.markdown("<h1 style='text-align: center; color: grey;'>RAYA</h1>",unsafe_allow_html=True)
 #image = Image.open('https://unsplash.com/photos/assorted-sliced-citrus-fruits-on-brown-wooden-chopping-board-1CsaVdwfIew')
 #streamlit.image(image, caption='Sunrise by the mountains')
-logo=streamlit.image("https://assets.mofoprod.net/network/images/Raya_fbtlwo_FMQwLtk.original.jpg",width=200)
+#logo=streamlit.image("https://assets.mofoprod.net/network/images/Raya_fbtlwo_FMQwLtk.original.jpg",width=200)
+
+logo=streamlit.image("https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",width=200)
 
 
 streamlit.title('My Parents New Healthy Diner!!!!')
